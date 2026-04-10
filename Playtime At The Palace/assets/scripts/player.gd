214f8f36aa1d6203e3098c80c63e4c8bc925565c
Ethan <80540543+eanderso2@users.noreply.github.com>
2026-04-10 18:21:48 -0400

    added basic pause

--- Playtime At The Palace/assets/scripts/player.gd
+++ Playtime At The Palace/assets/scripts/player.gd
@@ -15,16 +15,22 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	#movement
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		#movement
+		velocity.z = Input.get_axis("forward","back")*cos(rotation.y) - Input.get_axis("left","right")*sin(rotation.y)
+		velocity.x = Input.get_axis("forward","back")*sin(rotation.y) + Input.get_axis("left","right")*cos(rotation.y)
+		velocity = velocity*SPEED
 	
-	velocity.z = Input.get_axis("forward","back")*cos(rotation.y) - Input.get_axis("left","right")*sin(rotation.y)
-	velocity.x = Input.get_axis("forward","back")*sin(rotation.y) + Input.get_axis("left","right")*cos(rotation.y)
-	velocity = velocity*SPEED
 	
+		#looking
+		rotate_y(-Input.get_last_mouse_velocity().x / get_viewport().size.x /20)
+		$Camera3D.rotate_x(-Input.get_last_mouse_velocity().y / get_viewport().size.y / 20)
 	
-	#looking
-	rotate_y(-Input.get_last_mouse_velocity().x / get_viewport().size.x /20)
-	$Camera3D.rotate_x(-Input.get_last_mouse_velocity().y / get_viewport().size.y / 20)
+		#pause
+		if Input.is_action_just_pressed("pause"):
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	elif Input.is_action_just_pressed("pause"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 	move_and_slide()
